b47424582e1518a8ee70f51589609113d0d97092
Carolinek11 <iy334953@gmail.com>
2023-09-27 12:11:08 +0630

    epi21 error fixed

--- src/pages/api/user/index.ts
+++ src/pages/api/user/index.ts
@@ -4,7 +4,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const method = req.method;
   if (method === "GET") {
-    const users = await prisma.user.findMany();
+    const users = await prisma.user.findMany({
+      where: { isArchived: false },
+    });
     return res.send(users);
   } else if (method === "POST") {
     //Data Validation
@@ -13,6 +15,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!isValid) return res.status(400).send("Bad Request");
     const user = await prisma.user.create({ data: { name, email } });
     return res.send(user);
+  } else if (method === "PUT") {
+    const { id } = req.body;
+    await prisma.user.update({
+      data: { isArchived: true },
+      where: { id },
+    });
+    return res.send("OK");
   }
   res.status(405).send("Bad request");
 };
