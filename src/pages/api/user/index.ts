04a67eff4ae19c0c07ab3ace960da6169b689494
Carolinek11 <iy334953@gmail.com>
2023-09-27 09:08:45 +0630

    epi-21 w err

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/utils/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const method = req.method;
+  if (method === "GET") {
+    const users = await prisma.user.findMany();
+    return res.send(users);
+  } else if (method === "POST") {
+    //Data Validation
+    const { name, email } = req.body;
+    const isValid = email && name;
+    if (!isValid) return res.status(400).send("Bad Request");
+    const user = await prisma.user.create({ data: { name, email } });
+    return res.send(user);
+  }
+  res.status(405).send("Bad request");
+};
+
+export default handler;
